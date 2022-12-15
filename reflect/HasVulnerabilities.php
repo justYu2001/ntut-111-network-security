42081bc3bfc312ecafa5b094b34ba652b49caf18
Zixun55 <b0979433390@gmail.com>
2022-12-15 13:19:48 +0800

    feat: reflect

--- reflect/HasVulnerabilities.php
+++ reflect/HasVulnerabilities.php
@@ -3,12 +3,3 @@
     parse_str($test, $params);
     echo ' Hi '.$params['t'];
 ?>
-
-<html>
-
-<body>
-    <form action="" method="post">
-        <input type='text' id='name' name='name'>
-        <input type='submit' value='submit'>
-    </form>
-</html>
