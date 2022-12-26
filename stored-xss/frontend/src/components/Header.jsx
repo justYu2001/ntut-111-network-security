b3f3b5ac7880eff00be990c09ffc924a476d3447
justYu2001 <jack4781012@gmail.com>
2022-12-26 14:45:50 +0800

    feat: click logo and back to index page

--- stored-xss/frontend/src/components/Header.jsx
+++ stored-xss/frontend/src/components/Header.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <>
             <header className="bg-white shadow-md flex justify-between px-6 py-4 items-center fixed w-full">
-                <h1 className="text-xl">網路安全第七組期末 Demo - 儲存型 XSS</h1>
-                <a href="/user/new" className="bg-indigo-400 text-white font-medium px-4 py-2 rounded-md">新增使用者</a>
+                <h1 className="text-xl">
+                    <Link to="/">網路安全第七組期末 Demo - 儲存型 XSS</Link>
+                </h1>
+                <Link 
+                    to="/user/new"
+                    className="bg-indigo-400 text-white font-medium px-4 py-2 rounded-md"
+                >
+                    新增使用者
+                </Link>
             </header>
             <div className="h-24"></div>
         </>
