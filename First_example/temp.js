b4ed19a8438e7cfb4c6fc67bbf82e37ffe248839
l1ang0226 <ahuhwr886128@gmail.com>
2022-12-27 12:51:12 +0800

    First Example

---
+++ First_example/temp.js
@@ -0,0 +1,24 @@
+// let PureInput = document.querySelector('#PureInput');
+// let PureButton = document.querySelector('#PureButton');
+// let PureOutcome = document.querySelector('#PureOutcome');
+let unsafe_str = ''
+
+function escapeHTML (unsafe_str) {
+    if(!unsafe_str) return
+    return unsafe_str .replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;').replace(/\'/g, '&#39;').replace(/\//g, '&#x2F;')
+} //透過HTML的原始碼的字元實體
+
+PureButton.addEventListener('click', ()=>{
+    str = PureInput.value;
+    PureOutcome.innerHTML = str;
+})
+
+ReplaceButton.addEventListener('click', ()=>{
+    str = ReplaceInput.value;
+    ReplaceOutcome.innerHTML = escapeHTML(str);
+})
+
+TextButton.addEventListener('click', ()=>{
+    str = TextInput.value;
+    TextOutcome.innerText = str;
+})
